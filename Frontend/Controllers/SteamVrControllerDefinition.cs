e6feaddb1395d4186447e9c03d35c88730ff7e64
Jonathan Barnoud <jonathan@barnoud.net>
2024-01-10 14:52:47 +0100

    path3

--- Frontend/Controllers/SteamVrControllerDefinition.cs
+++ Frontend/Controllers/SteamVrControllerDefinition.cs
@@ -49,7 +49,7 @@ namespace Narupa.Frontend.Controllers
         /// </summary>
         public static SteamVrControllerDefinition GetControllerDefinition(string id)
         {
-            var resources = Resources.LoadAll<SteamVrControllerDefinition>("NarupaIMD");
+            var resources = Resources.LoadAll<SteamVrControllerDefinition>("Controllers");
             Array.ForEach(resources, Debug.Log);
             return resources.FirstOrDefault(type => type.controllerId == id);
         }
